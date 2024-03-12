9643affcec95c4a3bb2bd097a33c9d3255a9a018
Cardin-Ia <142541266+Cardin-Ia@users.noreply.github.com>
2024-03-12 16:57:55 +0000

    Tue, Mar 12, 2024, 9:57 AM -07:00

--- js/expanding.js
+++ js/expanding.js
@@ -13,3 +13,16 @@ function removeActiveClasses(){
         panel.classList.remove('active')
     })
 }
+
+nav = document.querySelector('.nav')
+
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    console.log(window.scrollY)
+    if (window.scrollY > 223) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
