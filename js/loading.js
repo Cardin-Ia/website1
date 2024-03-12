d6b494be7fe13cf1bddf7d68cec07999db288e97
Cardin-Ia <142541266+Cardin-Ia@users.noreply.github.com>
2024-03-12 16:38:31 +0000

    Tue, Mar 12, 2024, 9:38 AM -07:00

--- js/loading.js
+++ js/loading.js
@@ -16,3 +16,16 @@ function blurring() {
 
     bg.style.filter =  `blur(${30-load/100*30}px)`
 }
+
+nav = document.querySelector('.nav')
+
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    console.log(window.scrollY)
+    if (window.scrollY > 223) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
