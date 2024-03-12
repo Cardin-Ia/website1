e953842cec57455cf70638e324c3260e9d13aca9
Cardin-Ia <142541266+Cardin-Ia@users.noreply.github.com>
2024-03-12 16:55:57 +0000

    Tue, Mar 12, 2024, 9:55 AM -07:00

--- js/search.js
+++ js/search.js
@@ -4,3 +4,16 @@ search = document.querySelector('.search')
 btn.addEventListener('click', () => {
     search.classList.toggle('active')
 })
+
+nav = document.querySelector('.nav')
+
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    console.log(window.scrollY)
+    if (window.scrollY > 223) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
