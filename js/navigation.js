9f6a95ebf8003f793b8e53f1b2343f29f8344861
Cardin-Ia <142541266+Cardin-Ia@users.noreply.github.com>
2024-03-12 17:17:51 +0000

    Tue, Mar 12, 2024, 10:17 AM -07:00

--- js/navigation.js
+++ js/navigation.js
@@ -10,3 +10,16 @@ function fixNav() {
         nav.classList.remove('active')
     }
 }
+
+nav2 = document.querySelector('.nav2')
+
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    console.log(window.scrollY)
+    if (window.scrollY > 223) {
+        nav2.classList.add('active')
+    } else {
+        nav2.classList.remove('active')
+    }
+}
