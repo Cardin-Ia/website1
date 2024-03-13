a6e7aea0da3c0c45917405496c3099d3449930e7
Cardin-Ia <142541266+Cardin-Ia@users.noreply.github.com>
2024-03-13 16:47:37 +0000

    Wed, Mar 13, 2024, 9:47 AM -07:00

--- js/index.js
+++ js/index.js
@@ -8,10 +8,4 @@ btn.addEventListener('click', () => {
     var rgb = "rgb(" + randomNumber1 + "," + randomNumber2 + "," + randomNumber3 + ")";
     document.body.style.backgroundColor = rgb;
 
-    btn1.addEventListener('click', () => {
-        var HrandomNumber1 = Math.floor(Math.random() * 256);
-        var HrandomNumber2 = Math.floor(Math.random() * 256);
-        var HrandomNumber3 = Math.floor(Math.random() * 256);
-        var rgb1 = "rgb(" + HrandomNumber1 + "," + HrandomNumber2 + "," + HrandomNumber3 + ")";
-        document.body.style.backgroundColor = rgb1;
 })
